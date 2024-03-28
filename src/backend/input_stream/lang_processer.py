3cfa44c63f5902f35bda3e02227c4cb557cbdb70
jesse chen <yasha8155@gmail.com>
2024-03-27 23:53:53 -0700

    first of many edits of nlp..

--- src/backend/input_stream/lang_processer.py
+++ src/backend/input_stream/lang_processer.py
@@ -3,48 +3,53 @@ from nltk.tokenize import word_tokenize
 import spacy
 from spacy.matcher import PhraseMatcher
 
-nlp = spacy.load("en_core_web_sm")
-
 
 class small_language_processer():
     def __init__(self):
-        self.nlp = spacy.load("en_core_web_sm")
+        self.nlp = spacy.load("en_core_web_md")
+
+    def filter_allowed_string(self, s):
+        allowed = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+,.?:!$% ")
+        return ''.join(c for c in s if c in allowed)
 
     ## takes a string of set and returns interested topics/keywords
     def extract_keywords(self, text: str) -> set:
     # Removes hyphen and convert to lower
-        text = text.replace("-", " ").lower()
-        doc = nlp(text)
+        text = self.filter_allowed_string(text)
+        doc = self.nlp(text)
 
         keywords = set()
+        unwanted = set()
+
+        for ent in doc.ents:
+                if ent.label_ in {"EVENT", "FAC", "GPE", "LANGUAGE", "LAW", "LOC", "NORP", "ORG", "PERSON", "PRODUCT", "WORK_OF_ART"}:
+                    keywords.add(ent.text)
+                else:
+                    unwanted.add(ent.text)
 
         def is_single_symbol(keyword):
             return len(keyword) == 1 and not keyword.isalnum()
 
 
-        def contains_number(s):
-            return any(char.isdigit() for char in s)
-
-
-
-
-        # Extract named entities, excluding specific types and those containing numbers or emojis
-        for ent in doc.ents:
-            if ent.label_ not in {"MONEY", "DATE", "TIME", "QUANTITY"} and not contains_number(ent.text):
-                keywords.add(ent.text)
-
         for chunk in doc.noun_chunks:
-            if not any(token.pos_ in {'PRON', 'ADJ', 'INTJ', 'AUX', 'ADV', 'ADP'} or token.is_stop for token in chunk) and not any(token.is_punct or "-" in token.text for token in chunk) and not contains_number(chunk.text):
-                # Simplify chunks to the last two words if longer than two words
-                words = chunk.text.split()
-                simplified_chunk = ' '.join(words[-2:]) if len(words) > 2 else chunk.text
-                keywords.add(simplified_chunk)
 
-        # Extract additional nouns, excluding those that are part of noun chunks/entities, or contain numbers or emojis
-        for token in doc:
-            if token.pos_ == 'NOUN' and not token.is_punct and token.dep_ != "compound" and not contains_number(token.text) and not is_single_symbol(token.text):
-                is_part_of_chunk_or_entity = any(token.text in keyword for keyword in keywords)
-                if not is_part_of_chunk_or_entity:
-                    keywords.add(token.text)
+            clean_chunk = []
+
+            if chunk in unwanted:
+                continue
+
+            for token in chunk:
+                if token.pos_ == 'PRON':
+                    break
+
+                if token.pos_ in {'ADJ', 'NOUN', 'PROPN'}:
+                    clean_chunk.append(token.text)
+
+            if len(clean_chunk) > 2 or len(clean_chunk) == 0:
+                continue
+            else:
+                keyword = ' '.join(clean_chunk)
+                if not is_single_symbol(keyword):
+                    keywords.add(keyword)
 
         return keywords
